c8b3bb93d7b35f40cfba845dfa8e2a3722b8bb54
imakero <45946863+imakero@users.noreply.github.com>
2022-11-29 15:13:47 +0100

    fix linting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,9 @@ class ModelTests(TestCase):
         email = "test@example.com"
         password = "example123"
 
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -39,7 +41,9 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user("", "test123")
 
     def test_create_superuser(self):
-        user = get_user_model().objects.create_superuser("test@example.com", "test123")
+        user = get_user_model().objects.create_superuser(
+            "test@example.com", "test123"
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
