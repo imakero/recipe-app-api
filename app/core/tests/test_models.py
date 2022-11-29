00666f102dac2706bed1787fcd8bbfe39a754ddb
imakero <45946863+imakero@users.noreply.github.com>
2022-11-29 11:37:25 +0100

    add user custom model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,4 +16,19 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email, password="sample123"
+            )
+            self.assertEqual(user.email, expected)
